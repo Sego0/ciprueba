7ffc8adb82af92661a66549a86d93fcff44b5c9c
Sego0 <diego.segoviac@usm.cl>
2024-02-28 16:41:09 -0300

    bad line

--- CImanage/settings.py
+++ CImanage/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'ciprueba.postgres.database.azure.com',
+        'HOST': 'localhost',
         'PORT': '5432'
     }
 }
